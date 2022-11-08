db5c0e65a68ea14ed413dd26a0997131331e0a02
Enmanuel Proano <90797585+enmpro@users.noreply.github.com>
2022-11-08 11:32:12 -0500

    adds counter, guessing entire word, end win screen

--- hangman.py
+++ hangman.py
@@ -22,24 +22,43 @@ def playAgain():
             exit(0)
             
 def gameRun():
+    count = 0
     gameRunning = True
     wordsList = ["Python", "Java", "Programming"]
     guessCorrect = ["Nice Guess!", "Awesome!", "Good job!"]
     wordToGuess = random.choice(wordsList)
     wordGuessList = []
     wordGuessListUnderscore = []
+    
     for x in range(len(wordToGuess)):
         wordGuessList += wordToGuess[x]
         wordGuessListUnderscore += "_"
+    
+    numOfLetters = len(wordGuessList)
+    
     while gameRunning:
-            
+        
+        if count == numOfLetters:
+            print("\nYou win!")
+            print(f"You guessed the word {wordToGuess}!\n")
+            gameRunning = False
+            break
+        
         print("Guess the word: ", "".join(wordGuessListUnderscore))
-        userGuess = input("Enter a letter: ")
+        userGuess = input("Enter a letter (or word if you know it): ")
         if userGuess.isalpha() == True:
+            
+            if userGuess.lower() == wordToGuess.lower():
+                print("\nYou win!")
+                print(f"You guessed the word {wordToGuess}!\n")
+                gameRunning = False
+                break
+            
             for x in range(len(wordGuessList)):
                 if userGuess.lower() == wordGuessList[x].lower():
                     print(random.choice(guessCorrect))
                     wordGuessListUnderscore[x] = wordGuessList[x]
+                    count += 1
                     
         else:
             print("\nPlease enter a letter.\n")
